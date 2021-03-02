64d1463027afb0a89b0b9ab70240ad08a5155abf
Siamen_Silivonchyk <Siamen_Silivonchyk@epam.com>
2021-03-02 12:09:43 +0300

    commit after refactoring at 02-Mar-2021 second try

---
+++ LearningPythonWithPyCharm_LinkedIn/Module1/utilities/url_utilities.py
@@ -0,0 +1,13 @@
+def load_urls_from_file(file_path: str):
+    # TODO: add teh code needed to read the text filewoth the urls in it
+    pass
+
+
+def load_page(url: str):
+    # TODO: add the code that reads the url
+    pass
+
+
+def scrape_page(page_contents: str):
+    # TODO: analyze the text
+    pass
