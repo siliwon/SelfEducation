88cd0d2de292211d4868d1778b5f46af4c63cc22
Siamen_Silivonchyk <Siamen_Silivonchyk@epam.com>
2021-03-03 10:37:21 +0300

    commit at 10-36 03-Mar-2021 finished work with database through DB Browser (PyCharm Commutiny Edition is just fine)

--- LearningPythonWithPyCharm_LinkedIn/PageSpider/utilities/database_utilities.py
+++ LearningPythonWithPyCharm_LinkedIn/PageSpider/utilities/database_utilities.py
@@ -1,8 +1,35 @@
+import sqlite3 as lite
+
+
 def create_database(database_path: str):
-    # TODO: generate the database
-    pass
+    connection = lite.connect(database_path)
+    with connection:
+        cur = connection.cursor()
+        cur.execute("drop table if exists words")
+        ddl = 'CREATE TABLE "words" (' \
+              '"word" TEXT NOT NULL UNIQUE, ' \
+              '"usage_count"	INTEGER NOT NULL DEFAULT 1, ' \
+              'PRIMARY KEY("word"))'
+        cur.execute(ddl)
+        ddl = "CREATE UNIQUE INDEX words_word_uindex ON words (word)"
+        cur.execute(ddl)
+    connection.close()
 
 
 def save_words_to_database(database_path: str, words_list: list):
-    # TODO: save the words to the database
-    pass
+    connection = lite.connect(database_path)
+    with connection:
+        cur = connection.cursor()
+        for word in words_list:
+            # check to see if the word is in there
+            sql = "SELECT count(word) FROM words WHERE word='" + word + "'"
+            cur.execute(sql)
+            count = cur.fetchone()[0]
+            if count > 0:
+                sql = "UPDATE words SET usage_count = usage_count + 1 " \
+                      "WHERE word= '" + word + "'"
+            else:
+                sql = "INSERT INTO words(word) VALUES('" + word + "')"
+            cur.execute(sql)
+        # connection.close()
+        print("Database save complete!")
